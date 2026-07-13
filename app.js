9a8d1081e41e3e8aee29e93be88b229ca6bf7ee9
ChyyamakaU <amakaumunna90@gmail.com>
2026-07-13 12:06:03 +0100

    feature: add existing students

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 class StudenManagementSystem {
 
-constructor (fName, lName, email){
+static existingStudents= [
+    {firstName: "Philip", lastName: "Joesph", email:"phil@gmail.com", studentId:1, passWord:3201},
+      {firstName: "Grace", lastName: "Peter", email:"phil@gmail.com", studentId:2, passWord:1092},
+        {firstName: "Gift", lastName: "Joesph", email:"phil@gmail.com", studentId:3, passWord:8734},
+          {firstName: "Helwn", lastName: "Paa", email:"phil@gmail.com", studentId:4, passWord: 2359}
+]
 
+constructor (fName, lName, email){
 this.firstName =fName
 this.lastName =lName
 this.myEmail = email
-this.studentid= this.generatestudentid()
+this.studentId= this.generateStudentId()
+StudenManagementSystem.students.push(this)
 }
 
-generatestudentid(){
-    return Math.floor(Math.random()*100)
+generateStudentId(){
+    return Math.ceil(Math.random()*10)
 }
 
+// studentDashboard(surName){
+// return StudenManagementSystem[i]
+// }
+
+signIn(id, surName){
+if (this.studentId ===id && this.lastName === surName){
 
 
+}
 
-
-
+}
 
 
 
@@ -29,3 +42,4 @@ console.log(firstStudent)
 console.log(secondStudent)
 console.log(thirdStudent)
 console.log(fourtStudent)
+console.log(StudenManagementSystem.students)
