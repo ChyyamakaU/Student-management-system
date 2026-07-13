0644a2c15b913d39d4010e6dd18f0f894e160d68
ChyyamakaU <amakaumunna90@gmail.com>
2026-07-13 11:21:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+class StudenManagementSystem {
+
+constructor (fName, lName, email){
+
+this.firstName =fName
+this.lastName =lName
+this.myEmail = email
+this.studentid
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+let firstStudent =new StudenManagementSystem("Amaka", "Umu", "Amaka@gmail.com")
+let secondStudent =new StudenManagementSystem("John", "Doe", "John@gmail.com")
+let thirdStudent =new StudenManagementSystem("Jane", "James", "Jane@gmail.com")
+let fourtStudent =new StudenManagementSystem("Juliet", "Paul", "Juliet@gmail.com")
+console.log(firstStudent)
+console.log(secondStudent)
+console.log(thirdStudent)
+console.log(fourtStudent)
