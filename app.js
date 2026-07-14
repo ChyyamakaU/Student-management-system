7b2a3cffb8d40be76c4b0bc71a8254914313e35e
ChyyamakaU <amakaumunna90@gmail.com>
2026-07-14 15:47:47 +0100

    feature: add sign in function

--- app.js
+++ app.js
@@ -1,45 +1,62 @@
 class StudenManagementSystem {
 
-static existingStudents= [
-    {firstName: "Philip", lastName: "Joesph", email:"phil@gmail.com", studentId:1, passWord:3201},
-      {firstName: "Grace", lastName: "Peter", email:"phil@gmail.com", studentId:2, passWord:1092},
-        {firstName: "Gift", lastName: "Joesph", email:"phil@gmail.com", studentId:3, passWord:8734},
-          {firstName: "Helwn", lastName: "Paa", email:"phil@gmail.com", studentId:4, passWord: 2359}
-]
+    static existingStudents = [
+        { firstName: "Philip", lastName: "Joesph", email: "phil@gmail.com", studentId: "ST-001", passWord: 3201 },
+        { firstName: "Grace", lastName: "Peter", email: "gra@gmail.com", studentId: "ST-002", passWord: 1092 },
+        { firstName: "Gift", lastName: "Joesph", email: "gift@gmail.com", studentId: "ST-003", passWord: 8734 },
+        { firstName: "Helwn", lastName: "Paa", email: "Hel@gmail.com", studentId: "ST-004", passWord: 2359 }
+    ]
+    constructor(firstName, lastName, myEmail) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.myEmail = myEmail
+        // this.studentId = this.generateStudentId()
+        StudenManagementSystem.existingStudents.push(this)
+    }
+   
+//  generateStudentId (){
+//    const nextid=StudenManagementSystem.existingStudents.length+1
+//  }
 
-constructor (fName, lName, email){
-this.firstName =fName
-this.lastName =lName
-this.myEmail = email
-this.studentId= this.generateStudentId()
-StudenManagementSystem.students.push(this)
+    static signIn (id, passWord) {
+        for (const student of StudenManagementSystem.existingStudents) {
+            if (student.studentId === id && student.passWord === passWord) {
+                return student;
+            } else {return "Incorrect Student ID or name"}
+        }
+    }
+
+
+
+
+studentDashboard(){
+
+
 }
 
-generateStudentId(){
-    return Math.ceil(Math.random()*10)
-}
 
-// studentDashboard(surName){
-// return StudenManagementSystem[i]
+
+
+// static signUp(){
+// for(const student of StudenManagementSystem.existingStudents){
+// if(student.)
+
 // }
 
-signIn(id, surName){
-if (this.studentId ===id && this.lastName === surName){
+// }
 
 
-}
-
-}
 
 
 
 }
-let firstStudent =new StudenManagementSystem("Amaka", "Umu", "Amaka@gmail.com")
-let secondStudent =new StudenManagementSystem("John", "Doe", "John@gmail.com")
-let thirdStudent =new StudenManagementSystem("Jane", "James", "Jane@gmail.com")
-let fourtStudent =new StudenManagementSystem("Juliet", "Paul", "Juliet@gmail.com")
-console.log(firstStudent)
-console.log(secondStudent)
-console.log(thirdStudent)
-console.log(fourtStudent)
-console.log(StudenManagementSystem.students)
+// let firstStudent = new StudenManagementSystem("Amaka", "Umu", "Amaka@gmail.com")
+// let secondStudent = new StudenManagementSystem("John", "Doe", "John@gmail.com")
+// let thirdStudent =new StudenManagementSystem("Jane", "James", "Jane@gmail.com")
+// let fourtStudent =new StudenManagementSystem("Juliet", "Paul", "Juliet@gmail.com")
+// console.log(firstStudent)
+// console.log(secondStudent)
+// console.log(thirdStudent)
+// console.log(fourtStudent)
+// console.log(StudenManagementSystem.existingStudents)
+console.log(StudenManagementSystem.signIn("ST-001", 3201) )
