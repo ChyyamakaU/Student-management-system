e3d97ac269f84f30199f2ced35735af58cb1898d
ChyyamakaU <amakaumunna90@gmail.com>
2026-07-20 00:06:43 +0100

    feature: add change password method

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 studentsDatabase = [
-    { firstName: "Jane", lastName: "John", email: "jane@gmail.com", studentId: "STU-001", passWord: 3201, level: 100 },
-    { firstName: "Grace", lastName: "Peter", email: "gra@gmail.com", studentId: "STU-002", passWord: 1092, level: 200 },
-    { firstName: "Gift", lastName: "John", email: "gift@gmail.com", studentId: "STU-003", passWord: 8734, level: 300 },
-    { firstName: "Helwn", lastName: "Paa", email: "Hel@gmail.com", studentId: "STU-004", passWord: 2359, level: 400 }
+    { firstName: "Jane", lastName: "John", email: "jane@gmail.com", studentId: "STU-001", passWord: 3201, level: 100, grade: { maths: 0, english: 0, economics: 0 } },
+    { firstName: "Grace", lastName: "Peter", email: "gra@gmail.com", studentId: "STU-002", passWord: 1092, level: 200, grade: { maths: 0, english: 0, economics: 0 } },
+    { firstName: "Gift", lastName: "John", email: "gift@gmail.com", studentId: "STU-003", passWord: 8734, level: 300, grade: { maths: 0, english: 0, economics: 0 } },
+    { firstName: "Helwn", lastName: "Paa", email: "Hel@gmail.com", studentId: "STU-004", passWord: 2359, level: 400, grade: { maths: 0, english: 0, economics: 0 } }
 ]
 
 staffDatabase = [
@@ -22,18 +22,24 @@ class StudenManagementSystem {
         this.password = myPassword
         this.level = level
         this.isLoggedIn = false
+        this.grade = {
+            maths: 0,
+            english: 0,
+            economics: 0
+        }
         // this.studentId = this.generateStudentId()
         // StudenManagementSystem.existingStudents.push(this)
     }
 
     static signIn(email, passWord) {
-        let student = studentsDatabase.find(studentsIn => studentsIn.email === email && myPassword ===passWord)
+        let student = studentsDatabase.find(studentsIn => studentsIn.email === email && myPassword === passWord)
         if (!student) {
             return "You are not a student, kindly signup"
         }
-      else{  student.isLoggedIn = true
-        console.log(`Hello ${student.firstName} ${student.lastName}, Welcome to your Dashboard`)
-      }
+        else {
+            student.isLoggedIn = true
+            console.log(`Hello ${student.firstName} ${student.lastName}, Welcome to your Dashboard`)
+        }
         return student
 
     }
@@ -49,11 +55,24 @@ class StudenManagementSystem {
         return `Hello ${firstName} ${lastName}, You have sucessfully signed up, sign in now.`
     }
 
+    static changePassword(email, oldPassword, newPassword) {
+        let students = studentsDatabase.find(students => students.email === email);
+        if (!students) { return "Student email incorrect" };
+
+        if (students.passWord !== oldPassword) {
+            return "Incorrect Password, please enter the correct password"
+        }
+        else (students.passWord = newPassword)
+            return "Your password has been successfully changed"
+        }
+    
+
 }
 
 // console.log(StudenManagementSystem.signIn("phil@gmail.com", 3201))
 // console.log(StudenManagementSystem.signup("Amaka", "Umu", "Amaka@gmail.com", "STU-005", 1209, 100))
 // console.log(studentsDatabase)
+console.log(StudenManagementSystem.changePassword ("Hel@gmail.com", 8359, 4322))
 class StaffManagement {
 
     static signedInStaff = null;
@@ -65,55 +84,73 @@ class StaffManagement {
         this.staffId = staffId
         this.passWord = password
         this.isSignedIn = false
+
         // this.levelCoordinator = levelCoordinator
     }
 
     static signIn(email, password) {
-        let staff = staffDatabase.find(staffIn => staffIn.email === email && staffIn.passWord === password )
+        let staff = staffDatabase.find(staffIn => staffIn.email === email && staffIn.passWord === password)
         if (!staff) {
             return "You are not a staff, Kindly sign up"
         }
 
-       else {staff.isSignedIn = true;
-        StaffManagement.signedInStaff = staff;
-        console.log(`Hello ${staff.title} ${staff.firstName} ${staff.lastName}, Welcome to your Dashboard.`)
-       
+        else {
+            staff.isSignedIn = true;
+            StaffManagement.signedInStaff = staff;
+            console.log(`Hello ${staff.title} ${staff.firstName} ${staff.lastName}, Welcome to your Dashboard.`)
+
         }
-         return staff
+        return staff
     }
 
-static signUp (title, firstName, lastName, email, staffd, password){
-let staff =staffDatabase.find((staffUp)=> staffUp.email === email)
+    static signUp(title, firstName, lastName, email, staffd, password) {
+        let staff = staffDatabase.find((staffUp) => staffUp.email === email)
 
-if (staff) {return "You already have an existing account in the database, kindly signin"}
+        if (staff) { return "You already have an existing account in the database, kindly signin" }
 
-let newstaaff = new StaffManagement(title, firstName, lastName, email, staffd, password)
-staffDatabase.push(newstaaff)
+        let newstaaff = new StaffManagement(title, firstName, lastName, email, staffd, password)
+        staffDatabase.push(newstaaff)
 
-return `Hello ${title} ${firstName} ${lastName}, You have sucessfully signed up, sign in now.`
-}
-
-
-static viewStudents(){
-  
-if (!StaffManagement.signedInStaff) {
-        return "You are not signed in.";
+        return `Hello ${title} ${firstName} ${lastName}, You have sucessfully signed up, sign in now.`
     }
 
-    let myStudents = studentsDatabase.filter(student =>
-        student.level === StaffManagement.signedInStaff.levelCoordinator
-    );
-return myStudents
+
+    static viewStudents() {
+
+        if (!StaffManagement.signedInStaff) {
+            return "You are not signed in.";
+        }
+
+        let myStudents = studentsDatabase.filter(student =>
+            student.level === StaffManagement.signedInStaff.levelCoordinator
+        );
+        return myStudents
+    }
+
+
+    static updateStudentGrade(id, subject, score) {
+        if (!StaffManagement.signedInStaff) { return "You are not signed in" };
+
+        const mystudent = studentsDatabase.find(myStudents => myStudents.studentId === id)
+        if (!mystudent) { return "We do not have your record" }
+
+        if (mystudent.level !== StaffManagement.signedInStaff.levelCoordinator) {
+            return "You do not have access to update this record"
+        }
+
+        mystudent.grade[subject] = score;
+
+        return `Grade updated: ${subject} = ${score}`;
+
+    }
+
+
 }
 
 
-static updateRecord (id, levelCoordinator, ){}
-
-
-
-}
-
-console.log(StaffManagement.signIn("jum@gmail.com", 2943))
-console.log(StaffManagement.signUp("Miss", "Ada", "Igwe", "ada@gmail.com", "STA-006", 3671))
+// console.log(StaffManagement.signIn("jum@gmail.com", 2943))
+// console.log(StaffManagement.signUp("Miss", "Ada", "Igwe", "ada@gmail.com", "STA-006", 3671))
 // console.log(staffDatabase)
-console.log(StaffManagement.viewStudents())
+// console.log(StaffManagement.viewStudents())
+// console.log(StaffManagement.updateStudentGrade("STU-004", "maths", 89))
+// console.log(studentsDatabase)
