3f557941d86c71309317cbc55120b536db833dbb
ChyyamakaU <amakaumunna90@gmail.com>
2026-07-19 19:34:02 +0100

    fix: staff signin and sign up functions

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 studentsDatabase = [
-    { firstName: "Philip", lastName: "Joesph", email: "phil@gmail.com", studentId: "ST-001", passWord: 3201, level: 100 },
-    { firstName: "Grace", lastName: "Peter", email: "gra@gmail.com", studentId: "ST-002", passWord: 1092, level: 200 },
-    { firstName: "Gift", lastName: "Joesph", email: "gift@gmail.com", studentId: "ST-003", passWord: 8734, level: 300 },
-    { firstName: "Helwn", lastName: "Paa", email: "Hel@gmail.com", studentId: "ST-004", passWord: 2359, level: 400 }
+    { firstName: "Jane", lastName: "John", email: "jane@gmail.com", studentId: "STU-001", passWord: 3201, level: 100 },
+    { firstName: "Grace", lastName: "Peter", email: "gra@gmail.com", studentId: "STU-002", passWord: 1092, level: 200 },
+    { firstName: "Gift", lastName: "John", email: "gift@gmail.com", studentId: "STU-003", passWord: 8734, level: 300 },
+    { firstName: "Helwn", lastName: "Paa", email: "Hel@gmail.com", studentId: "STU-004", passWord: 2359, level: 400 }
 ]
 
- staff = [
-        { title: "Mr", firstName: "Danjuma", lastName: "Peter", email: "Danju@gmail.com", staffId: "ST001", passWord: 1923, levelCoordinator: 100 },
-        { title: "Dr", firstName: "Dan", lastName: "Paul", email: "Dan@gmail.com", staffId: "ST002", passWord: 1083, levelCoordinator: 200 },
-        { title: "Mr", firstName: "John", lastName: "Doel", email: "John@gmail.com", staffId: "ST003", passWord: 1424, levelCoordinator: 300 },
-        { title: "Dr", firstName: "Jumma", lastName: "Joel", email: "jum@gmail.com", staffId: "ST004", passWord: 2943, levelCoordinator: 400 }
-    ]
+staffDatabase = [
+    { title: "Mr", firstName: "Danjuma", lastName: "Peter", email: "Danju@gmail.com", staffId: "STA-001", passWord: 1923, levelCoordinator: 100 },
+    { title: "Dr", firstName: "Dan", lastName: "Paul", email: "Dan@gmail.com", staffId: "STA-002", passWord: 1083, levelCoordinator: 200 },
+    { title: "Mrs", firstName: "Jamila", lastName: "Doel", email: "John@gmail.com", staffId: "STA-003", passWord: 1424, levelCoordinator: 300 },
+    { title: "Dr", firstName: "Jumma", lastName: "Joel", email: "jum@gmail.com", staffId: "STA-004", passWord: 2943, levelCoordinator: 400 }
+]
 
 class StudenManagementSystem {
 
@@ -27,14 +27,14 @@ class StudenManagementSystem {
     }
 
     static signIn(email, passWord) {
-        let student = studentsDatabase.find(studentsIn=> studentsIn.email=== email) 
-            if (!student){
-return "You are not a student, kindly signup"
+        let student = studentsDatabase.find(studentsIn => studentsIn.email === email)
+        if (!student) {
+            return "You are not a student, kindly signup"
         }
-        student.isLoggedIn =true 
+        student.isLoggedIn = true
         console.log(`Hello ${student.firstName} ${student.lastName}, Welcome to your Dashboard`)
         return student
-    
+
     }
 
     static signup(firstName, lastName, myEmail, myID, passWord, level) {
@@ -43,49 +43,62 @@ return "You are not a student, kindly signup"
         if (student) { return "Your details already exists in the database, kindly signin." }
         // else { return "Lets get you signed up!!" }
 
-     let newStudent =   new StudenManagementSystem(firstName, lastName, myEmail, myID, passWord, level)
-studentsDatabase.push(newStudent)
-        return "You have sucessfully signed up, sign in now"
+        let newStudent = new StudenManagementSystem(firstName, lastName, myEmail, myID, passWord, level)
+        studentsDatabase.push(newStudent)
+        return `Hello ${firstName} ${lastName}, You have sucessfully signed up, sign in now.`
     }
 
 }
-// let secondStudent = new StudenManagementSystem("John", "Doe", "John@gmail.com")
-// let thirdStudent =new StudenManagementSystem("Jane", "James", "Jane@gmail.com")
-// let fourtStudent =new StudenManagementSystem("Juliet", "Paul", "Juliet@gmail.com")
-// console.log(firstStudent)
-// console.log(secondStudent)
-// console.log(thirdStudent)
-// console.log(fourtStudent)
 
-console.log(StudenManagementSystem.signIn("phil@gmail.com", 3201))
-console.log(StudenManagementSystem.signup("Amaka", "Umu", "Amaka@gmail.com", "ST005", 1209, 100))
+// console.log(StudenManagementSystem.signIn("phil@gmail.com", 3201))
+console.log(StudenManagementSystem.signup("Amaka", "Umu", "Amaka@gmail.com", "STU-005", 1209, 100))
 console.log(studentsDatabase)
 
 
 
-// class StaffManagement {
-   
+class StaffManagement {
 
-//     constructor(title, firstName, lastName, email, staffId, passWord, levelCoordinator) {
-//         this.title = title
-//         this.firstName = firstName
-//         this.lastName = lastName
-//         this.email = email
-//         this.staffId = staffId
-//         this.passWord = passWord
-//         this.levelCoordinator = levelCoordinator
-//     }
+    constructor(title, firstName, lastName, email, staffId, password) {
+        this.title = title
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.staffId = staffId
+        this.passWord = password
+        this.isLoggedIn = false
+        // this.levelCoordinator = levelCoordinator
+    }
 
-//     static login(Id, passWord) {
-//         for (let theStaff of StaffManagement.staff) {
+    static signIn(email, password) {
+        let staff = staffDatabase.find(staffIn => staffIn.email === email && staffIn.passWord === password)
+        if (!staff) {
+            return "You are not a staff, Kindly sign up"
+        }
 
-//             if (theStaff.staffId === id && theStaff.levelCoordinator === passWord) {
-//                 return staff
-//             } else { "You do nothave acess to this page" }
-//         }
+        staff.isLoggedIn = true
+        console.log(`Hello ${staff.title} ${staff.firstName} ${staff.lastName}, Welcome to your Dashboard.`)
+        return staff
+    }
 
-//     }
+static signUp (title, firstName, lastName, email, staffd, password){
+let staff =staffDatabase.find((staffUp)=> staffUp.email === email)
 
-// }
+if (staff) {return "You already have an existing account in the database, kindly signin"}
 
-// console.log(StaffManagement.login("ST002", 2943))
+let newstaaff = new StaffManagement(title, firstName, lastName, email, staffd, password)
+staffDatabase.push(newstaaff)
+
+return `Hello ${title} ${firstName} ${lastName}, You have sucessfully signed up, sign in now.`
+
+}
+
+
+
+
+
+
+}
+
+// console.log(StaffManagement.signIn("jum@gmail.com", 2943))
+// console.log(StaffManagement.signUp("Miss", "Ada", "Igwe", "ada@gmail.com", "STA-006", 3671))
+// console.log(staffDatabase)
