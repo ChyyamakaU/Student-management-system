ab58fb1584904e3658a900fcf0acc49e114bb697
ChyyamakaU <amakaumunna90@gmail.com>
2026-07-20 13:15:08 +0100

    add: api fetch task

---
+++ fetch.js
@@ -0,0 +1,65 @@
+// async function fetchPost() {
+
+//     const controller = new AbortController();
+
+
+//     const timeout = setTimeout(() => {
+//         controller.abort(new Error("controller aborted took too long"))
+//     }, 2000)
+
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//             signal: controller.signal
+//         })
+
+//         let res = await response.json()
+//         let count = 0;
+//         for (let i = 0; i < res.length; i++) {
+
+//             if (res[i].body) { count++ }
+
+//         }
+
+//         console.log(`The total number of posts are ${count}`)
+
+//         // console.log (res)
+//     }
+//     catch (e) {
+//         console.log(e.message)
+//     }
+// }
+// fetchPost()
+
+
+
+// 2
+
+
+import { readFile } from "node:fs/promises"
+
+async function downloadData() {
+
+    try {
+        let [student, courses, grade] = await Promise.all(
+            [
+                readfile("student.json", "utf8"),
+                readfile("courses.json", "utf8"),
+                readfile("grade.json", "utf8")
+            ]
+        );
+
+        const myStudent = JSON.parse(student);
+        const myCourses = JSON.parse(courses);
+        const myGrade = JSON.parse(grade);
+
+
+        console.log(myStudent.name)
+        console.log(myCourses.length)
+        console.log(myGrade.length)
+    }
+    catch (e) {
+        console.log(e.message)
+    }
+}
+
+downloadData()
